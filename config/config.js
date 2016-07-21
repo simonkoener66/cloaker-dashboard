c3a6e766c9e7f99e0f3a19f88ade56bef73d58f3
Simon Koener <simon.koener66@outlook.com>
2016-07-21 11:04:25 +0200

    .env introduced

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+	port: process.env.PORT,
+	loginUrl: process.env.LOGINURL,
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	googleLoginRedirectUrl: process.env.GOOGLE_LOGIN_REDIRECT_URL,
+	databaseConnection: process.env.DB_CONNECTION,
+};
